04bf2ecd5b89f3e233f48a67b0bf86ff40a11b59
aaron <aaron@daw.com>
2024-02-07 18:53:29 +0000

    Conf inicial

---
+++ vite.config.js
@@ -0,0 +1,24 @@
+import { defineConfig } from "vite";
+import path from 'path';
+import vue from "@vitejs/plugin-vue";
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [vue()],
+  server: {
+    port: 3010,
+  },
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "src"),
+      "@public": path.resolve(__dirname, "public"),
+    },
+  },
+  css: {
+    preprocessorOptions: {
+      scss: {
+        additionalData: `@import "@/styles/variables.scss";`, // Importa tus variables SCSS globales aquí
+      },
+    },
+  }
+});
