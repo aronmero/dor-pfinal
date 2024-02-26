f9359f05b07f72b709272e46cea288ae6d61b678
aronmero <aronmedinasp@gmail.com>
2024-02-26 14:24:48 +0000

    Ajuste de variables en sass

--- src/main.js
+++ src/main.js
@@ -2,7 +2,7 @@ import { createApp } from "vue";
 import { createPinia } from 'pinia'
 import router from "@/router/index.js";
 import App from "./App.vue";
-import "./style.css";
+import "./styles/default.scss";
 import "./styles/style.scss";
 
 async function enableMocking() {
