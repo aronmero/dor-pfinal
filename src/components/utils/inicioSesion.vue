6900244de073f8acb8484394d75ab72e89c6c5cc
Daniel Delgado <danidp25.08@gmail.com>
2024-02-27 19:35:21 +0000

    Add-> Componente InicioSesion e implementacion en vista

---
+++ src/components/utils/inicioSesion.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <form action="">
+      <input type="text" name="" id="inicio-sesion-user">
+      <input type="password" name="" id="inicio-sesion-password">
+      <input type="submit" value="">
+    </form>
+  </div>
+</template>
+<script>
+</script>
+<style>
+  
+</style>
--- src/views/singIn.vue
+++ src/views/singIn.vue
@@ -1,11 +1,12 @@
 <template>
   <Header/>
-
+  <InicioSesion/>
   <Footer/>
 </template>
 <script setup>
 import Header from "@/components/header.vue";
 import Footer from "@/components/footer.vue";
+import InicioSesion from "@/components/utis/inicioSesion.vue";
 </script>
 <style scoped>
   
