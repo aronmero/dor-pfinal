f2c4dc4eeda3d8a667e51f6d71bf4bdbc468d2ac
Arturo407LP <arturo.argual@gmail.com>
2024-02-28 18:31:36 +0000

    solucionado problemas vista busqueda version movil

--- src/views/search.vue
+++ src/views/search.vue
@@ -6,38 +6,38 @@ import BotonPublicar from "@/components/utils/botonPublicar.vue";
 </script>
 
 <template>
-    <Header />
-    <div class="content">
-        <div class="barra-busqueda">
-            <input type="search" placeholder="Busqueda...">
-        </div>
-        <div class="selector">
-            <div class="seccion"><a href="#">Productos</a></div>
-            <div class="seccion"><a href="#">Empresas</a></div>
-            <div class="seccion"><a href="#">Hastags</a></div>
-            <div class="seccion"><a href="#" id="seleccionado">En tu Zona</a>
-                <hr />
+        <Header />
+        <div class="content">
+            <div class="barra-busqueda">
+                <input type="search" placeholder="Busqueda...">
+            </div>
+            <div class="selector">
+                <div class="seccion"><a href="#">Productos</a></div>
+                <div class="seccion"><a href="#">Empresas</a></div>
+                <div class="seccion"><a href="#">Hastags</a></div>
+                <div class="seccion"><a href="#" id="seleccionado">En tu Zona</a>
+                    <hr />
+                </div>
+            </div>
+
+            <div class="mapa">
+                <iframe
+                    src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d5888.115828841858!2d-17.908705227037455!3d28.656885439999712!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0xc6bf232aa032949%3A0x28c5cb4e0babc907!2sIES%20Eusebio%20Barreto%20Lorenzo!5e0!3m2!1ses!2ses!4v1709061998345!5m2!1ses!2ses"
+                    allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
             </div>
         </div>
 
-
-        <div class="mapa">
-            <iframe
-                src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d5888.115828841858!2d-17.908705227037455!3d28.656885439999712!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0xc6bf232aa032949%3A0x28c5cb4e0babc907!2sIES%20Eusebio%20Barreto%20Lorenzo!5e0!3m2!1ses!2ses!4v1709061998345!5m2!1ses!2ses"
-                allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
-        </div>
-    </div>
-
-    <BotonPublicar />
-    <Footer />
+        <BotonPublicar />
+        <Footer />
 </template>
 
 <style scoped lang="scss">
+
 .content {
     display: flex;
     flex-direction: column;
+    justify-content: start;
     align-items: center;
-    justify-content: space-around;
     margin-bottom: 100px;
 }
 
@@ -79,34 +79,25 @@ import BotonPublicar from "@/components/utils/botonPublicar.vue";
             width: 100%;
         }
 
-        #seleccionado{
+        #seleccionado {
             color: white;
         }
     }
 }
 
-.mapa{
+.mapa {
     border: 2px solid black;
     width: 50%;
     height: 600px;
     margin: 100px;
 
-    iframe{
+    iframe {
         height: 100%;
         width: 100%;
     }
 }
 
-@media screen and (max-width: 1400px) {
-    .content {
-        padding-top: 25px;
-    }
-}
-
 @media screen and (max-width: 1200px) {
-    .content {
-        margin-top: 25px;
-    }
 
     .barra-busqueda {
         width: 100%;
@@ -120,16 +111,13 @@ import BotonPublicar from "@/components/utils/botonPublicar.vue";
         }
     }
 
-    .mapa{
+    .mapa {
         width: 80%;
-        height: 500px;
+        height: 600px;
     }
 }
 
 @media screen and (max-width: 768px) {
-    .content {
-        margin-top: 25px;
-    }
 
     .barra-busqueda {
         width: 100%;
@@ -143,9 +131,9 @@ import BotonPublicar from "@/components/utils/botonPublicar.vue";
         }
     }
 
-    .mapa{
+    .mapa {
         width: 80%;
-        height: 400px;
+        height: 550px;
     }
 }
 </style>
